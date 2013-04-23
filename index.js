f0cbea91fdc321c055284400f6f8b51c29e1dfaa
zhangyunlong <zhangyunlong@baidu.com>
2013-04-23 21:21:52 +0800

    fis升级为全局对象

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-function normalize(str, defaultExt, fis){
+function normalize(str, defaultExt){
     var info = fis.util.stringQuote(str);
     var rest = info.rest.trim();
     var pathinfo = fis.util.ext(rest);
@@ -18,11 +18,11 @@ function normalize(str, defaultExt, fis){
     return info.quote + rest + info.quote;
 }
 
-function analyseJs(content, file, fis){
+function analyseJs(content, file){
     var reg = /"(?:[^\\"]|\\[\s\S])+"|'(?:[^\\']|\\[\s\S])+'|\/\/[\r\n]+|\/\*[\s\S]+?\*\/|\brequire\s*\(\s*("(?:[^\\"]|\\[\s\S])+"|'(?:[^\\']|\\[\s\S])+')\s*\)/g;
     content = content.replace(reg, function(m, value){
         if(value){
-            value = normalize(value, '.js', fis);
+            value = normalize(value, '.js');
             var info = fis.uri.getId(value, file.dirname);
             file.addRequire(info.id);
             m = 'require(' + info.quote + info.id + info.quote + ')';
@@ -35,20 +35,20 @@ function analyseJs(content, file, fis){
     return content;
 }
 
-function analyseCss(content, file, fis){
+function analyseCss(content, file){
     var reg = /@require\s+('[^']+'|"[^"]+"|[^\s{}]+)[\s;]*/g;
     return content.replace(reg, function(m, value){
-        value = normalize(value, '.css', fis);
+        value = normalize(value, '.css');
         var info = fis.uri.getId(value, file.dirname);
         file.addRequire(info.id);
         return '';
     });
 }
 
-module.exports = function(content, file, conf, fis){
+module.exports = function(content, file, conf){
     if(file.rExt === '.js'){
-        return analyseJs(content, file, fis);
+        return analyseJs(content, file);
     } else if(file.rExt === '.css'){
-        return analyseCss(content, file, fis);
+        return analyseCss(content, file);
     }
 };
